e7cbc94f7e1bcd4d6efc599483ef7fff71cfaff7
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-09-21 01:09:34 +0530

    Updated ScrollingEffect

--- Assets/Scripts/Level/InfiniteScrollingBackground.cs
+++ Assets/Scripts/Level/InfiniteScrollingBackground.cs
@@ -6,12 +6,33 @@ public class InfiniteScrollingBackground : MonoBehaviour
 {
     public float speed = 0.1f;
     public GameObject background1;
-    //public GameObject background2;
-    public Vector3 posb1;
-    public Vector3 posb2;
+    public GameObject background2;
 
-    private void Update()
+    private float backgroundHeight;
+
+    void Start()
     {
-        background1.transform.Translate(Vector2.down*speed*Time.deltaTime);
+        backgroundHeight = background1.GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update()
+    {
+        MoveBackground(background1);
+        MoveBackground(background2);
+
+        if (background1.transform.position.y <= -backgroundHeight)
+        {
+            background1.transform.position = new Vector2(background1.transform.position.x, background2.transform.position.y + backgroundHeight);
+        }
+
+        if (background2.transform.position.y <= -backgroundHeight)
+        {
+            background2.transform.position = new Vector2(background2.transform.position.x, background1.transform.position.y + backgroundHeight);
+        }
+    }
+
+    void MoveBackground(GameObject background)
+    {
+        background.transform.position += Vector3.down * speed * Time.deltaTime;
     }
 }
